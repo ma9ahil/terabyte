ce49707e3afc03e7641478e6e75fc4d95af6b695
Thorsten Papenbrock <papenbrock@informatik.uni-marburg.de>
2025-06-13 17:27:35 +0200

    Added a test case to the PositionListIndexTest to test for a re-curring error that involves intersect-implementations not considering -1 values as null values.

--- src/test/java/de/di/data_profiling/structures/PositionListIndexTest.java
+++ src/test/java/de/di/data_profiling/structures/PositionListIndexTest.java
@@ -2,7 +2,7 @@ package de.di.data_profiling.structures;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class PositionListIndexTest {
 
@@ -21,5 +21,19 @@ public class PositionListIndexTest {
         assertEquals(pli3.getClusters(), pli2.intersect(pli1).getClusters());
         assertEquals(pli4.getClusters(), pli1.intersect(pli2).intersect(pli5).getClusters());
         assertEquals(pli7.getClusters(), pli1.intersect(pli6).intersect(pli3).getClusters());
+    }
+
+    @Test
+    public void testNullValueNotClustered() {
+        PositionListIndex pli1 = new PositionListIndex(new AttributeList(new int[]{0}), new String[]{"a","a","b","b","c"});
+        PositionListIndex pli2 = new PositionListIndex(new AttributeList(new int[]{0}), new String[]{"a","a","a","b","b"});
+        PositionListIndex pli3 = new PositionListIndex(new AttributeList(new int[]{0}), new String[]{"c","d","a","a","a"});
+
+        PositionListIndex pli12 = pli1.intersect(pli2);
+        PositionListIndex pli13 = pli1.intersect(pli3);PositionListIndex pli123 = pli12.intersect(pli13);
+
+        assertFalse(pli12.isUnique());
+        assertFalse(pli13.isUnique());
+        assertTrue(pli123.isUnique());
     }
 }
